c15f394a239848c18075af4b15ef2e54a9d74b2a
Guillaume Potier <cobalt2760@gmail.com>
2014-04-03 18:36:25 +0200

    - added users list filtering

--- pipewatch.js
+++ pipewatch.js
@@ -60,11 +60,25 @@
       return this.api.get('users')
         .done($.proxy(function (response) {
           this.users = response.data;
-          this.usersIds = {};
+
+          if (this.options.unmonitored_users.length)
+            this.users = this.users.filter($.proxy(function (user) {
+              return -1 === this.options.unmonitored_users.indexOf(user.email);
+            }, this));
+
+          // sort users array email DESC
+          this.users.sort(function (a, b) {
+            if (a.email === b.email)
+              return 0;
+
+            return a.email > b.email ? 1 : -1;
+          });
 
           // generate fake everyone user
           this.users.unshift({ id: -1, email: 'everyone@wisembly.com' });
 
+          // generate users dictionary
+          this.usersIds = {};
           for (var i = 0; i < this.users.length; i++)
             this.usersIds[this.users[i].id] = i;
 
@@ -481,3 +495,37 @@ function ucfirst (string) {
     return data ? fn(data) : fn;
   };
 })();
+
+// lte IE8 compatibility
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf)
+  Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+      "use strict";
+      if (this === null) {
+          throw new TypeError();
+      }
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (len === 0) {
+          return -1;
+      }
+      var n = 0;
+      if (arguments.length > 1) {
+          n = Number(arguments[1]);
+          if (n != n) { // shortcut for verifying if it's NaN
+              n = 0;
+          } else if (n !== 0 && n != Infinity && n != -Infinity) {
+              n = (n > 0 || -1) * Math.floor(Math.abs(n));
+          }
+      }
+      if (n >= len) {
+          return -1;
+      }
+      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+      for (; k < len; k++) {
+          if (k in t && t[k] === searchElement) {
+              return k;
+          }
+      }
+      return -1;
+  };
