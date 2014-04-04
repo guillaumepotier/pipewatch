a0dd016f7d7c941a16995cde1ec50e8ba02cce80
Guillaume Potier <cobalt2760@gmail.com>
2014-04-04 12:48:01 +0200

    - updated config.js

--- config.js
+++ config.js
@@ -1,5 +1,26 @@
 window.PipeWatchConfig = {
   api_token: null,
   pipeline_id: 4,
-  default_view: 'pipeline'
+  default_view: 'pipeline',
+  unmonitored_users: [
+    'charlotte@wisembly.com',
+    'charlotte@balloonup.com',
+    'christina@balloonup.com',
+    'cobalt2760@gmail.com',
+    'emmanuel@wisembly.com',
+    'romain@balloonup.com',
+    'selma@wisembly.com',
+    'selma@balloonup.com',
+    'sebastien@wisembly.com'
+  ],
+  timeline_options: {
+    amount: [1, 3, 6],    // interval numbers
+    start: [0, 1, 3, 6],  // now - amount * monts
+    interval: ['month', 'quarter']
+  },
+  timeline: {
+    amount: 6,
+    start: 0,
+    interval: 'month'
+  }
 };
